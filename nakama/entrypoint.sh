729b23f778337605e60e83664b2a79621edd1ad3
Amulya Rawat <amulyarawat87@gmail.com>
2026-04-06 20:06:59 +0530

    Revert to stable state: CORS fixed, auth working, modules in default location

--- nakama/entrypoint.sh
+++ nakama/entrypoint.sh
@@ -11,5 +11,10 @@ echo "▶ Running Nakama DB migrations..."
 
 echo "▶ Starting Nakama server..."
 exec /nakama/nakama \
-  --config /nakama/nakama.yml \
-  --database.address "$DATABASE_URL"
+  --database.address "$DATABASE_URL" \
+  --logger.level INFO \
+  --socket.server_key "defaultkey" \
+  --runtime.http_key "defaulthttpkey" \
+  --console.port 7351 \
+  --console.username "admin" \
+  --console.password "password"
