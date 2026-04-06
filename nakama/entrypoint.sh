2c91493ffd271ba4a067e77dba7b75d9105ec260
Amulya Rawat <amulyarawat87@gmail.com>
2026-04-06 17:02:34 +0530

    Fix CORS: use YAML config instead of invalid CLI flags

--- nakama/entrypoint.sh
+++ nakama/entrypoint.sh
@@ -15,9 +15,6 @@ exec /nakama/nakama \
   --logger.level INFO \
   --socket.server_key "defaultkey" \
   --runtime.http_key "defaulthttpkey" \
-  --session.token_expiry_sec 7200 \
-  --session.http_cors_allowed_origins "https://tic-tac-toe-flax-one-36.vercel.app,http://localhost:3000" \
-  --session.http_cors_max_age 3600 \
   --console.port 7351 \
   --console.username "admin" \
   --console.password "password"
